f48f7996d42b48fef771c229c708478081c49a57
Lukasz <lukasz.krawiec@appliedtechnology.se>
2023-01-06 15:38:05 +0100

    Added working put request with reloadCustomer()

--- frontend/functions.js
+++ frontend/functions.js
@@ -13,7 +13,15 @@ function createCustomerList(data) {
     for (let i = 0; i < data.length; i++){
         const customerH1 = document.createElement('h1');
 
-        customerH1.textContent = `${data[i].name} ${data[i].age}`;
+        const updateButton = document.createElement('button');
+        updateButton.setAttribute('class', 'updateButton');
+        const buttonText = document.createTextNode("Update customer");
+        updateButton.appendChild(buttonText);
+        updateButton.setAttribute('property', data[i].id);
+        updateButton.onclick = function() {updateCustomer(updateButton.getAttribute('property'))};
+        
+        customerList.appendChild(updateButton);
+        customerH1.textContent = `Name: ${data[i].name} Email: ${data[i].email} Age: ${data[i].age}`;
         customerH1.setAttribute('property', data[i].id)
         customerList.appendChild(customerH1);
     }
@@ -25,13 +33,80 @@ function search() {
     filter = input.value.toUpperCase();
     div = document.getElementById('myDiv');
     h1 = div.getElementsByTagName('h1');
+    buttons = div.getElementsByTagName('button');
 
     for(let i = 0; i < h1.length; i++) {
         txtValue = h1[i].textContent;
         if(txtValue.toUpperCase().indexOf(filter) > -1) {
             h1[i].style.display = "";
+            buttons[i].style.display = "";
         } else {
             h1[i].style.display = "none";
+            buttons[i].style.display = "none";
         }
     }
+}
+function reloadCustomer(Customer) {
+    var myDiv = document.getElementById('myDiv');
+    var customers = myDiv.getElementsByTagName('h1');
+    console.log(customers);
+    for(let i = 0; i < customers.length; i++){
+        if(customers[i].getAttribute('property') == Customer.id){
+            customers[i].textContent = "";
+            customers[i].textContent = `Name: ${Customer.name} 
+            Email: ${Customer.email} 
+            Age: ${Customer.age}`
+        }
+    }
+    alert("Customer updated!");
+}
+function updateCustomer(customerID) {
+    const Customer = {
+        id: customerID,
+        name: document.getElementById('customerName').value,
+        email: document.getElementById('customerEmail').value,
+        age: document.getElementById('customerAge').value
+    }
+    const putRequest = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Customer)
+    }
+    fetch('http://localhost:8080/api/v1/customers', putRequest);
+    reloadCustomer(Customer);
+}
+function addCustomer(customerID) {
+    const Customer = {
+        id: customerID,
+        name: document.getElementById('customerName').value,
+        email: document.getElementById('customerEmail').value,
+        age: document.getElementById('customerAge').value
+    }
+    const postRequest = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Customer)
+    }
+    fetch('http://localhost:8080/api/v1/customers', postRequest);
+    addNewCustomer(Customer);
+}
+function addNewCustomer(Customer){
+    var myDiv = document.getElementById('myDiv');
+    const customerH1 = document.createElement('h1');
+
+    const updateButton = document.createElement('button');
+    updateButton.setAttribute('class', 'updateButton');
+    const buttonText = document.createTextNode("Update customer");
+    updateButton.appendChild(buttonText);
+    updateButton.setAttribute('property', data[i].id);
+    updateButton.onclick = function() {updateCustomer(updateButton.getAttribute('property'))};
+    
+    myDiv.appendChild(updateButton);
+    customerH1.textContent = `Name: ${data[i].name} Email: ${data[i].email} Age: ${data[i].age}`;
+    customerH1.setAttribute('property', data[i].id)
+    myDiv.appendChild(customerH1);
 }
